8ff32649356b22cefa63a53a5b96e2da967bf607
20223073 <20223073@kookmin.ac.kr>
2024-06-04 10:25:22 +0900

    Signed-off-by: 20223073 <20223073@kookmin.ac.kr>

--- common/apps.py
+++ common/apps.py
@@ -4,7 +4,4 @@ from django.db.models import BigAutoField
 
 class CommonConfig(AppConfig):
     name = 'common'
-
-class PyboConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pybo'
