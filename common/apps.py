cae5c11632e28f9c1cf782647fcc1764639616d8
20223073 <20223073@kookmin.ac.kr>
2024-06-04 10:14:37 +0900

    make migrations

--- common/apps.py
+++ common/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
+from django.db.models import BigAutoField
 
 
 class CommonConfig(AppConfig):
     name = 'common'
+
+class PyboConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pybo'
